87f5a6505b8e6e66b7805247a4c2690f4b088000
ehowland <eric.howland@gmail.com>
2014-11-20 23:37:52 -0600

    simplified

--- minecraft/walkwall.py
+++ minecraft/walkwall.py
@@ -7,14 +7,16 @@ air = 0
 stone = 1
 gold = 41
 wall_height = 5
-lastx, lasty, lastz = mc.player.getPos()
 
-while True:
-  x, y, z = mc.player.getPos()
-  mc.setBlocks(x-1, y, z-1, x+1, y+10, z+1, air)
-  mc.setBlocks(lastx, lasty,lastz, lastx, lasty+5, lastz, gold)
-  mc.setBlock(lastx-1, lasty-1, lastz-1, lastx+1, lasty-1, lastz+1, stone)
-  lastx = x
-  lasty = y
-  lastz = z
-  sleep(0.1)
+
+sleep(10)
+
+firstx, firsty, firstz = mc.player.getPos()
+mc.postToChat('Go')
+
+sleep(10)
+
+endx, endy, endz = mc.player.getPos()
+mc.setBlocks(firstx, firsty,firstz, endx, endy+wall_height, endz, gold)
+mc.postToChat('Building')
+mc.player.setPos(endx+1,endy,endz+1)
