c0d506cc59fc170b682aa02a51796049ca698058
ehowland <eric.howland@gmail.com>
2014-11-20 18:33:39 -0600

    Initial programs for Girls INC
    Hello is from the Raspberry Pi website - just the minimum to get started
    
    flowers.py is the flower example from the Ras Pi website
    
    bulldozer.py clears the area around and above
    
    walkwall.py lets you walk along and create a wall behind you.

---
+++ minecraft/hello.py
@@ -0,0 +1,3 @@
+from mcpi import minecraft
+mc = minecraft.Mineraft.create()
+mc.postToChat('Hello world')
---
+++ minecraft/walkwall.py
@@ -0,0 +1,18 @@
+from mcpi import minecraft
+mc = minecraft.Mineraft.create()
+
+air = 0
+stone = 1
+gold = 41
+wall_height = 5
+lastx, lasty, lastz = mc.player.getPos()
+
+while True:
+  x, y, z = mc.player.getPos()
+  mc.setblocks(x-1, y, z-1, x+1, y+10, z+1, air)
+  mc.setblocks(lastx, lasty,lastz, lastx, lasty+5, lastz, gold)
+  mc.setblock(lastx-1, lasty-1, lastz-1, lastx+1, lasty-1, lastz+1, stone)
+  lastx = x
+  lasty = y
+  lastz = z
+  sleep(0.1)
