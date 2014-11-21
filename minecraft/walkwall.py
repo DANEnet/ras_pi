f906a22697d9596273af8daf3dcae91baa8a7cee
ehowland <ehowland@danenet.org>
2014-11-20 20:32:40 -0600

    more typos

--- minecraft/walkwall.py
+++ minecraft/walkwall.py
@@ -11,9 +11,9 @@ lastx, lasty, lastz = mc.player.getPos()
 
 while True:
   x, y, z = mc.player.getPos()
-  mc.setblocks(x-1, y, z-1, x+1, y+10, z+1, air)
-  mc.setblocks(lastx, lasty,lastz, lastx, lasty+5, lastz, gold)
-  mc.setblock(lastx-1, lasty-1, lastz-1, lastx+1, lasty-1, lastz+1, stone)
+  mc.setBlocks(x-1, y, z-1, x+1, y+10, z+1, air)
+  mc.setBlocks(lastx, lasty,lastz, lastx, lasty+5, lastz, gold)
+  mc.setBlock(lastx-1, lasty-1, lastz-1, lastx+1, lasty-1, lastz+1, stone)
   lastx = x
   lasty = y
   lastz = z
